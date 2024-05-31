452aae0a4206ce3f2f07f45c07d3136176f5df96
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2024-05-31 06:40:16 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,13 @@
 #pascal triangle 
 
+def pascal(n):
+    row = [1]
+    for x in range(n):
+        row.append(int(row[x] * (n-x) / (x+1)))
+    return row
+    
+
 def pascal_triangle(n):
-  pass
+    if n <= 0:
+        return []
+    return [pascal(nth) for nth in range(n)]
