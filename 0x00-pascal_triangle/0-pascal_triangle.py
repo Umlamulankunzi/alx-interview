c79f63883e9fec5d4827195c0feae2d962153373
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2024-05-31 07:05:05 +0200

    Update to conform to pep8 standards 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -32,7 +32,6 @@ Example Usage:
 """
 
 
-
 def pascal(n):
     """
     Generates a single row of Pascal's Triangle.
@@ -45,9 +44,9 @@ def pascal(n):
     """
     row = [1]
     for x in range(n):
-        row.append(int(row[x] * (n-x) / (x+1)))
+        row.append(int(row[x] * (n - x) / (x + 1)))
     return row
-    
+
 
 def pascal_triangle(n):
     """
