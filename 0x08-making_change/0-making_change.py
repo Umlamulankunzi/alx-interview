ddb1618ac70422d47a384e91d496c8ea223c1bba
Umlamulankunzi <studentjele@gmail.com>
2024-07-25 13:43:35 -0700

    fixed code to confirm to pep8 & made script exec

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
 Module that contains a function to determine the fewest number of coins
-needed to meet a given total amount when given a pile of coins of different values.
+needed to meet given total amount when given a pile of coins of
+different values
 """
+
 
 def makeChange(coins, total):
     """
@@ -23,14 +25,15 @@ def makeChange(coins, total):
     remainder = total
     coin_count = 0
     sorted_coins = sorted(coins, reverse=True)
-    
+
     for coin in sorted_coins:
         coin_count += remainder // coin
         remainder = remainder % coin
         if remainder == 0:
             return coin_count
-            
+
     return -1
 
+
 if __name__ == "__main__":
-    print(make_change([1, 2, 25], 37))
+    print(makeChange([1, 2, 25], 37))
